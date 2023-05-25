d7757475ed17822ec2f272bb476babe4bc9bfeea
judicael-florent <florentngaliembou@gmail.com>
2023-05-25 20:26:05 +0200

    phase de test

---
+++ lib/login_page.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//import 'package:travel_planner/welcome_page.dart';
+import 'welcome_page.dart';
+import 'delayed_animation.dart';
+import 'main.dart';
+import 'register.dart';
+
+class LoginPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          /*AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white.withOpacity(0),
+        leading: IconButton(
+          icon: Icon(
+            Icons.close,
+            color: Colors.black,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),*/
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('C:/Users/JUDICAEL-FLORENT/Desktop/COURS 3IL/Travel_Planner/images/back.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 40,
+                horizontal: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  DelayedAnimation(
+                    delay: 0,
+                    child: Text(
+                      "Connect you with your Identifier. "
+                          "if you don't have ther register you",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 22),
+                  DelayedAnimation(
+                    delay: 0,
+                    child: Text(
+                      "It's recommended to connect your username and your password"
+                          " for us to better protect your information. "
+                          "If you don't have an Account then click on register",
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+            SizedBox(height: 95),
+            LoginForm(),
+            SizedBox(height: 125),
+            DelayedAnimation(
+              delay: 0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  primary: d_purpose,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 125,
+                    vertical: 13,
+                  ),
+                ),
+                child: Text(
+                  'CONFIRM',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            DelayedAnimation(
+              delay: 500,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  primary: d_purpose,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 125,
+                    vertical: 15,
+
+                  ),
+                ),
+                child: Text(
+                  'REGISTER',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    height: 0.75,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterPage(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 30),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.only(right: 35),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WelcomePage(),
+                      ),
+                    );
+                  },
+                  child: DelayedAnimation(
+                    delay: 0,
+                    child: Text(
+                      "BACK",
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+        ],
+    ),
+
+    );
+  }
+}
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  var _obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 30,
+      ),
+      child: Column(
+        children: [
+          DelayedAnimation(
+            delay: 500,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                labelStyle: TextStyle(
+                  color: Colors.grey[400],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          DelayedAnimation(
+            delay: 500,
+            child: TextField(
+              obscureText: _obscureText,
+              decoration: InputDecoration(
+                labelStyle: TextStyle(
+                  color: Colors.grey[400],
+                  fontWeight: FontWeight.bold,
+                ),
+                labelText: 'Password',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.visibility,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+ 
---
+++ lib/register.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'delayed_animation.dart';
+import 'main.dart';
+import 'login_page.dart';
+
+
+class RegisterPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+
+      AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white.withOpacity(0),
+        leading: IconButton(
+          icon: Icon(
+            Icons.close,
+            color: Colors.black,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('C:/Users/JUDICAEL-FLORENT/Desktop/COURS 3IL/Travel_Planner/images/back.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 40,
+                horizontal: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  DelayedAnimation(
+                    delay: 0,
+                    child: Text(
+                      "REGISTER",
+                      style: GoogleFonts.poppins(
+                        color: d_purpose,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 22),
+                  DelayedAnimation(
+                    delay: 0,
+                    child: Text(
+                      "Choose a Username and Password "
+                          "that you want to use for this app ",
+                      style: GoogleFonts.poppins(
+                        color: Colors.grey[600],
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+            SizedBox(height: 35),
+            LoginForm(),
+            SizedBox(height: 125),
+            DelayedAnimation(
+              delay: 0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  primary: d_purpose,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 125,
+                    vertical: 13,
+                  ),
+                ),
+                child: Text(
+                  'REGISTER',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            SizedBox(height: 90),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.only(right: 35),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginPage(),
+                      ),
+                    );
+                  },
+                  child: DelayedAnimation(
+                    delay: 0,
+                    child: Text(
+                      "BACK",
+                      style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+        ],
+      ),
+    );
+  }
+}
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  var _obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 30,
+      ),
+      child: Column(
+        children: [
+          SizedBox(height: 70),
+          DelayedAnimation(
+            delay: 0,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Firstname',
+                labelStyle: TextStyle(
+                  color: Colors.grey[400],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 35),
+          DelayedAnimation(
+            delay: 0,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                labelStyle: TextStyle(
+                  color: Colors.grey[400],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          DelayedAnimation(
+            delay: 0,
+            child: TextField(
+              obscureText: _obscureText,
+              decoration: InputDecoration(
+                labelStyle: TextStyle(
+                  color: Colors.grey[400],
+                  fontWeight: FontWeight.bold,
+                ),
+                labelText: 'Password',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.visibility,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
